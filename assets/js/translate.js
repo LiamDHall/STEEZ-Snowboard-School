acb0210027fa3bdd6d9bd5dedd8cd39df18ef7b2
LiamDHall <hall.d.liam@gmail.com>
2020-08-14 15:25:42 +0000

    Move all third party scripts into assets
    This commit will move all scripts into their own folder
    and will be link to on all pages.

---
+++ assets/js/translate.js
@@ -0,0 +1,26 @@
+//Google Translate Script Source: https://jsfiddle.net/solodev/0stLrpqg/
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT}, 'google_translate_element');
+    }
+
+    function triggerHtmlEvent(element, eventName) {
+    var event;
+    if (document.createEvent) {
+        event = document.createEvent('HTMLEvents');
+        event.initEvent(eventName, true, true);
+        element.dispatchEvent(event);
+    } else {
+        event = document.createEventObject();
+        event.eventType = eventName;
+        element.fireEvent('on' + event.eventType, event);
+    }
+    }
+
+    jQuery('.lang-select').click(function() {
+    var theLang = jQuery(this).attr('data-lang');
+    jQuery('.goog-te-combo').val(theLang);
+
+    //alert(jQuery(this).attr('href'));
+    window.location = jQuery(this).attr('href');
+    location.reload();
+});
